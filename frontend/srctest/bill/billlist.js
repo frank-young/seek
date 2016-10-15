e2e43310966a2801a0704c3952eacf340ab2bc77
Frank Young <yangjunalns@qq.com>
2016-10-16 01:36:21 +0800

    结算

--- frontend/srctest/bill/billlist.js
+++ frontend/srctest/bill/billlist.js
@@ -2,8 +2,8 @@
  *                                                     订单列表
  ********************************************************************************************************************/
 
-angular.module("billlistMoudle", []).controller('BilllistCtrl', ['$scope','$window','orderData',
-  	function($scope,$window,orderData) {
+angular.module("billlistMoudle", []).controller('BilllistCtrl', ['$scope','$window','orderData','dishData',
+  	function($scope,$window,orderData,dishData) {
 
 		$window.document.title = "订单列表"; 
 		orderData.getData().then(function(data){
@@ -11,12 +11,36 @@ angular.module("billlistMoudle", []).controller('BilllistCtrl', ['$scope','$wind
 		})
 		$scope.payTypeArr = ['现金','微信','支付宝','会员卡','次卡']
 
+		$scope.cookAll = []
+		dishData.getData().then(function(data){
+			$scope.cookAll = data.dishs
+		})
+
 		$scope.lookAll = function(id){
 			orderData.getIdData(id).then(function(data){
 				$scope.order = data.order
 			})
 
 		}
+		
+		// 反位结算，删除本单，重新下单
+		$scope.againAccount = function(value){
+
+			localStorage.cook = JSON.stringify(value.dish)
+			localStorage.peopleNum = value.peopleNum
+			value.dish.forEach(function(v1,i1){
+				$scope.cookAll.forEach(function(v2,i2){
+					if(v1.name == v2.name){
+						v2.checked = true
+					}
+				})
+			})
+			localStorage.cookAll = JSON.stringify($scope.cookAll)
+			orderData.deleteData(value._id).then(function(data){
+				console.log(data)
+			})
+		}
+
 		
 	}
 ])
