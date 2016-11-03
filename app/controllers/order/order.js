e4dec3a40ec9af15575329eb70d739c073fb844c
Frank Young <yangjunalns@qq.com>
2016-11-03 17:32:46 +0800

    打印

--- app/controllers/order/order.js
+++ app/controllers/order/order.js
@@ -497,40 +497,28 @@ var Order = require('../../models/order/order'),	//引入模型
 
 	// 打印模块
 	exports.print = function(req,res){
-		var printer = require("printer"),
-		    util = require('util'),
-		    printerName = 'Foxit Reader PDF Printer',
-		    printerFormat = 'TEXT';
+		var printer = require('printer')
+		
+		var printer = require('./printer/node_printer_win32_x64.node')
+		var fs = require('fs')
 
-		printer.printDirect({
-		    data:"print from Node.JS buffer", // or simple String: "some text"
-			printer:printerName, // printer name
-			type: printerFormat, // type: RAW, TEXT, PDF, JPEG, .. depends on platform
-		    options: // supported page sizes may be retrieved using getPrinterDriverOptions, supports CUPS printing options
-		    {
-		        media: 'Letter',
-		        'fit-to-page': true
+		var info = fs.readFileSync('seek.txt').toString()
+
+		function sendPrint() {
+		  printer.printDirect({
+		    data: info,
+		    type: 'RAW',
+		    success: function (jobID) {
+		      console.log("ID: " + jobID)
 		    },
-			success:function(jobID){
-				console.log("sent to printer with ID: "+jobID);
-		        var jobInfo = printer.getJob(printerName, jobID);
-		        console.log("current job info:"+util.inspect(jobInfo, {depth: 10, colors:true}));
-		        if(jobInfo.status.indexOf('PRINTED') !== -1)
-		        {
-		            console.log('too late, already printed');
-		            return;
-		        }
-		        console.log('cancelling...');
-		        var is_ok = printer.setJob(printerName, jobID, 'CANCEL');
-				console.log("cancelled: "+is_ok);
-				try{
-					console.log("current job info:"+util.inspect(printer.getJob(printerName, jobID), {depth: 10, colors:true}));
-				}catch(err){
-					console.log('job deleted. err:'+err);
-				}
-			},
-			error:function(err){console.log(err);}
-		});
+		    error: function (err) {
+		      console.log('printer module error: '+err)
+		      throw err
+		    }
+		  })
+		}
+
+		sendPrint()
 	}
 
 
