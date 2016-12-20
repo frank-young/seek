dfd0a25b06e10e1dbaf85f62bdfa55e94704ce0d
frank <yangjunalns@qq.com>
2016-12-20 21:09:44 +0800

    菜品数据构建

--- app/controllers/mobile/mobile.js
+++ app/controllers/mobile/mobile.js
@@ -10,10 +10,14 @@ let Cook = require('../../models/dish/cook'),
 
 //构建微信点餐数据
 exports.goods = (req, res) => {
-	let domain = "seek01"
+    res.header("Access-Control-Allow-Origin", "*");
+
+    const HOT_NUM = 10
+    const HOT_NAME = '热销菜品'
+
+	let domain = "seek02"
 	let arr = []
     
-
     async.waterfall([
         (cb) => {
         	Cate.fetch({"domainlocal": domain},(err,cates) => {
@@ -24,7 +28,12 @@ exports.goods = (req, res) => {
         			obj.foods = []
         			arr.push(obj)
         		})
-
+                let hotObj = {
+                    "name": HOT_NAME,
+                    "type": 2,
+                    "foods": []
+                }
+                arr.unshift(hotObj)
 				cb(null, arr)
 			})
         },
@@ -34,54 +43,74 @@ exports.goods = (req, res) => {
                 M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1)
 
             Cook.fetch({ "domainlocal": domain }, (err, goods) => {
-                let goodsArr = []
-            	goods.forEach((good) => {
-                    // ((good) => {
-                        Item.fetch({ "domainlocal": domain,"name": good.name, "year": Y, "month":M}, (err, items) => {
+                let len = goods.length
+
+                for(let i = 0; i< len; i++){
+                    ((i) => {
+                        Item.fetch({ "domainlocal": domain,"name": goods[i].name, "year": Y, "month":M}, (err, items) => {
+                            let itemsLen = items.length,
+                                count = 0
+                            for(let j = 0;j < itemsLen; j++) {
+                                count += items[j].number
+
+                                if(count >= HOT_NUM) {
+                                    let hotGoods = {
+                                        "name": items[j].name,
+                                        "price": items[j].price,
+                                        "sellCount": count,
+                                        "description": ""
+                                    }
+                                    arr[0].foods.push(hotGoods)
+                                }
+                            }
+
                             let obj = {}
-                            obj.name = good.name
-                            obj.price = good.price
-                            obj.sellCount = items.length
-                            obj.description = good.description
-                            goodsArr.push(obj)
+                            let cateNum = parseInt(goods[i].cate) + 1
+                            obj.name = goods[i].name
+                            obj.price = goods[i].price
+                            obj.sellCount = count
+                            obj.description = goods[i].description
+                            arr[cateNum].foods.push(obj)
+
+                            if(i == len-1) {
+                                cb(null, arr)
+                            }
                         })
-                    // })(good)
-            	})
-
-                cb(null, goodsArr)
-		    })
-            
-        }
-        /*,
-        (arr, cb) => {
-            let date = new Date(),
-                Y = date.getFullYear(),
-                M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1)
-
-            Cook.fetch({ "domainlocal": domain }, (err, goods) => {
-                goods.forEach((good) => {
-                    Item.fetch({ "domainlocal": domain,"name": good.name, "year": Y, "month":M}, (err, items) => {
-                        let obj = {}
-                        obj.name = good.name
-                        obj.price = good.price
-                        obj.sellCount = items.length
-                        obj.description = good.description
-                        arr[good.cate].foods.push(obj)
-                    })
+                    })(i)
+                }
+		    })   
+        },
+        (arr,cb) => {
+            // 热销菜品的添加
+            Cate.fetch({"domainlocal": domain},(err,cates) => {
+                cates.forEach((cate) => {
+                    let obj = {}
+                    obj.name = cate.label
+                    obj.type = -1
+                    obj.foods = []
+                    arr.push(obj)
                 })
+
                 cb(null, arr)
             })
-            
-        }*/
+        }
     ], (err, result) => {
-        console.log('err => ' + err)
-        res.json({
-        	errno: 0,
-        	data:result
-        })
+        if(err){
+            res.json({
+                errno: 1000,
+                data:"发生错误"
+            })
+        } else {
+            res.json({
+                errno: 0,
+                data:result
+            })
+
+        }
     })
     
 }
+
 
 
 
